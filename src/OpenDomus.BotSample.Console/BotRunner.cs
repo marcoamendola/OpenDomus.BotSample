e216c4c4aba8ba919aa898a3d55fce674745bb92
Marco Amendola <marco.amendola@gmail.com>
2016-04-22 10:32:24 +0200

    initial setup

---
+++ src/OpenDomus.BotSample.Console/BotRunner.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDomus.BotSample.Console
+{
+    public class BotRunner
+    {
+
+        Action<Message> displayBotMessage;
+        Func<string> getUserMessage;
+        public BotRunner(Action<Message> displayBotMessage, Func<string> getUserMessage)
+        {
+            this.displayBotMessage = displayBotMessage;
+            this.getUserMessage = getUserMessage;
+        }
+
+        public void Run<R>(Func<IDialog<R>> rootDialogFactory)
+        {
+            var message = new Message()
+            {
+                ConversationId = Guid.NewGuid().ToString(),
+                Text = ""
+            };
+
+            while (true)
+            {
+                message = Conversation
+                    .SendAsync(message, rootDialogFactory)
+                    .Result;
+
+                if (message.Text == null) return;
+
+                displayBotMessage(message);
+
+                message.Text = getUserMessage();
+            }
+        }
+    }
+}
---
+++ src/OpenDomus.BotSample.Core/Dialogs/CustomerServiceDialog.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenDomus.BotSample.Core.Dialogs
+{
+    [LuisModel("49d7e5a7-beb4-40c2-9e01-e8a0cdebdc17", "0e50270c574844cb85480394ad946b4d")]
+    [Serializable]
+    public class CustomerServiceDialog : LuisDialog<object>
+    {
+
+        public CustomerServiceDialog()
+        {
+
+        }
+
+
+        [LuisIntent("")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = "Scusa, non ho capito.";
+            await context.PostAsync(message);
+
+            context.Wait(MessageReceived);
+        }
+
+
+        
+    }
+}
